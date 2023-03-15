eeed9ce471367233132fa4ed27a5755e0c7e2be2
Manuel Fernández <manferlo81@gmail.com>
2023-03-16 00:20:54 +0700

    Add "Mute" menu

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1464,7 +1464,7 @@
 #endif // HAS_MARLINUI_MENU
 
 #if ANY(HAS_DISPLAY, DWIN_LCD_PROUI, DWIN_CREALITY_LCD_JYERSUI)
-// #define SOUND_MENU_ITEM   // Add a mute option to the LCD menu
+#define SOUND_MENU_ITEM  // Add a mute option to the LCD menu
 #define SOUND_ON_DEFAULT // Buzzer/speaker default enabled state
 #endif
 
