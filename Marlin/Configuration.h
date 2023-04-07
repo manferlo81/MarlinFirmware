95cd52db7f42d5a5877169cafca31a959793cbd2
Manuel Fernández <manferlo81@gmail.com>
2023-04-07 13:11:25 +0700

    Move X_MIN according to my printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1865,7 +1865,7 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 2.3
+#define X_MIN_POS 4
 #define Y_MIN_POS -4
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 15
